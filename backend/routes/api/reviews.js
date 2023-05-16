c5fdaea6719e09d328f3bb8a05d66074c319ab18
CharlesMich <charleedongo@hotmail.com>
2023-05-16 12:44:10 -0500

    get/1 and 2

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const Sequelize = require('sequelize');
+
+const { Spot, Review, SpotImage, User } = require('../../db/models');
+
+const { requireAuth } = require('../../utils/auth');
+const router = express.Router();
+
+// Get all Reviews of the Current User
+router.get('/current', requireAuth, async (req,res,next)=> {
+   const userid = req.user.id;
+
+   const reviewById = await Review.findByPk(userid,{
+   include: [{model: User, attributes: ['id','firstName', 'lastName']},
+           {model: Spot}]
+
+
+   });
+   
+
+   res.json({"Reviews":[reviewById]});
+})
+
+
+
+module.exports = router;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,65 +2,115 @@ const express = require('express');
 
 const Sequelize = require('sequelize');
 
-const { Spot, Review, SpotImage } = require('../../db/models');
+const { Spot, Review, SpotImage, User } = require('../../db/models');
 
 const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
 
 // Get all Spots owned by the Current User
-router.get('/current', (req, res)=> {
-let user = req.user;
-console.log(user)
-   })
+router.get('/current', requireAuth, async (req, res) => {
 
-
+    let userid = req.user.id
+    const spotbyId = await Spot.findByPk(userid, {
+        attributes: {
+            include: [
+                [
+                    Sequelize.fn("AVG", Sequelize.col("Reviews.stars")),
+                    "avgRating"
+                ],
+            ],
+        },
+        include: [{
+            model: Review,
+            attributes: []
+        },
+        ]
+    })
+    res.json(spotbyId)
+})
 
 
 // Get details of a Spot from an id
-router.get('/:id', async (req, res)=> {
+router.get('/:id', async (req, res) => {
 
-const spotbyId = await Spot.findByPk(req.params.id,{
-include: {model:SpotImage}
+    const spotbyId = await Spot.findByPk(req.params.id, {
+
+    
+
+
+        attributes: {
+            include: [
+
+                [
+                    Sequelize.fn("AVG", Sequelize.col("Reviews.stars")),
+                    "avgStarRating"
+                ],
+                [Sequelize.fn("COUNT", Sequelize.col("Reviews.id")), "numReviews"]
+            ],
+        },
+        include: [
+            { model: SpotImage, attributes:['id', 'url', 'preview'] },
+         {model: User, attributes: ['id', 'firstName', 'lastName']},
+         {model: Review, attributes: [] }
+         
+        ]
+    })
+   let spot = spotbyId.toJSON();
+   console.log(spot)
+    spot['Owner'] = spot['User'];
+delete spot[ 'User' ];
+console.log(spot)
+    res.json(spot)
 })
-res.json(spotbyId)
-})
-
-
-
 
 
 // Get all Spots
 
-router.get('/', async (req, res)=> {
-// return res.json('app is listening')
+router.get('/', async (req, res) => {
+    // return res.json('app is listening')
     const spots = await Spot.findAll({
-        
         attributes: {
-            include: [ 
+            include: [
                 [
-                  Sequelize.fn("AVG", Sequelize.col("Reviews.stars")), 
-                  "avgRating"
-                ], 
-                // {"previewImage":  {model: SpotImage, attributes:['url']}}
+                    Sequelize.fn("AVG", Sequelize.col("Reviews.stars")),
+                    "avgRating"
+                ],
             ],
-          
-           
         },
         include: [{
-            model: Review, 
-            attributes: [] 
+            model: Review,
+            attributes: []
         },
-        {model: SpotImage, attributes:['url'], subQuery:false},
-        
+        {
+            model: SpotImage,
+            // attributes:[]
+        }
 
-     {model: SpotImage, attributes:[]},
-    ]
-       
-        //
+        ]
+
+
     })
-    // console.log(spots)
-    res.json({Spots:spots})
+    let spotList = [];
+    spots.forEach(list => {
+        spotList.push(list.toJSON())
+    })
+    // console.log(spotList)
+
+    spotList.forEach(list => {
+        list.SpotImages.forEach(img => {
+            if (img.preview === true) {
+
+                list.previewImage = img.url
+            }
+        })
+
+    })
+    spotList.forEach(ele => {
+        delete ele.SpotImages
+    })
+
+    res.json({ spots: spotList })
 })
 
 module.exports = router;
