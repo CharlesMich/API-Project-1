1a8c182408b84b1960836f4d62b7f23ebf7f1b1c
CharlesMich <charleedongo@hotmail.com>
2023-05-16 19:42:32 -0500

    fixed options

--- backend/db/migrations/20230512161031-create-user.js
+++ backend/db/migrations/20230512161031-create-user.js
@@ -15,6 +15,16 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull:false,
+      },
+
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull:false,
+      },
+
       username: {
         type: Sequelize.STRING,
         allowNull:false,
