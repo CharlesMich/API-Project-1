6c988a3d937cdfa67d40afafbb34c46fd840d231
CharlesMich <charleedongo@hotmail.com>
2023-05-21 10:48:28 -0500

    latitude/longitude fixed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -80,36 +80,36 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
     }
 })
 
-const validateupDate = [
-    check('address')
-        .exists({ checkFalsy: true })
-        .withMessage("Street address is required"),
-    check('city')
-        .exists({ checkFalsy: true })
-        .withMessage("City is required"),
-    check('state')
-        .exists({ checkFalsy: true })
-        .withMessage("State is required"),
-    check('country')
-        .exists({ checkFalsy: true })
-        .withMessage("Country is required"),
-    check('lat')
-        .exists({ checkFalsy: true })
-        .withMessage("Latitude is not valid"),
-    check('lng')
-        .exists({ checkFalsy: true })
-        .withMessage("Longitude is not valid"),
-    check('name')
-        .exists({ checkFalsy: true })
-        .withMessage("Name must be less than 50 characters"),
-    check('description')
-        .exists({ checkFalsy: true })
-        .withMessage("Description is required"),
-    check('price')
-        .exists({ checkFalsy: true })
-        .withMessage("Price per day is required"),
-    handleValidationErrors
-];
+// const validateupDate = [
+//     check('address')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Street address is required"),
+//     check('city')
+//         .exists({ checkFalsy: true })
+//         .withMessage("City is required"),
+//     check('state')
+//         .exists({ checkFalsy: true })
+//         .withMessage("State is required"),
+//     check('country')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Country is required"),
+//     check('lat')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Latitude is not valid"),
+//     check('lng')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Longitude is not valid"),
+//     check('name')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Name must be less than 50 characters"),
+//     check('description')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Description is required"),
+//     check('price')
+//         .exists({ checkFalsy: true })
+//         .withMessage("Price per day is required"),
+//     handleValidationErrors
+// ];
 
 // Get all Bookings for a Spot based on the Spot's id
 router.get('/:spotId/bookings', requireAuth, async (req, res) => {
@@ -142,7 +142,7 @@ router.get('/:spotId/bookings', requireAuth, async (req, res) => {
 })
 
 // Edit a Spot
-router.put('/:spotId', requireAuth, validateupDate, async (req, res) => {
+router.put('/:spotId', requireAuth, async (req, res) => {
     const currentSpot = req.params.spotId;
     const userId = req.user.id;
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
@@ -294,14 +294,14 @@ router.post('/', requireAuth, async (req, res, next) => {
             error: "Country is required"
         })
     }
-    if (!lat || isNaN(lat)) {
+    if (!lat) {
         res.statusCode = 400
         return res.json({
             message: "Bad Request",
             error: "Latitude is not Valid"
         })
     }
-    if (!lng || isNaN(lat)) {
+    if (!lng) {
         res.statusCode = 400
         return res.json({
             message: "Bad Request",
