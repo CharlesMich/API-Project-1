505002d95f56842ee3ebeb2e2b08ee863ea150f3
CharlesMich <charleedongo@hotmail.com>
2023-06-27 00:18:34 -0500

    commented out reviews

--- backend/db/migrations/20230514155141-create-review-image.js
+++ backend/db/migrations/20230514155141-create-review-image.js
@@ -18,11 +18,11 @@ module.exports = {
       reviewId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: {
-          model: "Reviews",
-          // key: "id"
+        // references: {
+        //   model: "Reviews",
+        //   // key: "id"
 
-        },
+        // },
         onDelete: 'CASCADE'
 
       },
